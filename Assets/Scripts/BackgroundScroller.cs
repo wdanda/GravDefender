da0316cc749e4d7db2010644dd4b14bde5811092
Wagner Da Silva <wdasilva@morneaushepell.com>
2020-09-27 09:24:05 -0400

    Scrolling bg

---
+++ Assets/Scripts/BackgroundScroller.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundScroller : MonoBehaviour
+{
+    [SerializeField] private float backgroundScrollSpeed = .5f;
+
+    Material myMaterial;
+    Vector2 offset;
+
+    void Start()
+    {
+        myMaterial = GetComponent<Renderer>().material;
+        offset = new Vector2(0, backgroundScrollSpeed);
+    }
+
+    void Update()
+    {
+        myMaterial.mainTextureOffset += offset * Time.deltaTime;        
+    }
+}
